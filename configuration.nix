6e64186006248bdd2ac7298a2967d918e3ca5d63
SirSouza <sr.souza.contato@gmail.com>
2026-01-09 23:03:03 -0300

    Initial NixOS configuration

---
+++ configuration.nix
@@ -0,0 +1,260 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running 'nixos-help').
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+    ./disks.nix # Arquivo local (ver disks.nix.example)
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.timeout = 120;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "NixOS";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Habilita suporte a OpenGL/Gráficos
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # Carrega o driver nvidia
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+  programs.fish.enable = true;
+
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+# PostgreSQL configurado para DEV (trust, sem senha)
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    ensureDatabases = [ "mydatabase" "anorak" ];
+    ensureUsers = [
+      {
+        name = "anorak";
+        ensureDBOwnership = true;
+      }
+    ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser     auth-method
+      local all       all        trust
+      host  all       all        127.0.0.1/32   trust
+      host  all       all        ::1/128        trust
+    '';
+  };
+
+  # --- CONFIGURAÇÃO DE USUÁRIO DO SISTEMA ---
+  users.users.anorak = {
+    isNormalUser = true;
+    description = "Anorak";
+    shell = pkgs.fish;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
+  };
+
+  # --- HOME MANAGER ---
+  home-manager.users.anorak =
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
+    let
+      gruvbox-theme-custom = pkgs.gruvbox-gtk-theme.override {
+        tweakVariants = [ "macos" ];
+        colorVariants = [ "dark" ];
+      };
+    in
+    {
+      home.stateVersion = "25.11";
+      home.enableNixpkgsReleaseCheck = false;
+
+      # Ativa o dconf (necessário para as configs abaixo)
+      dconf.enable = true;
+
+      dconf.settings = {
+        # Configurações de Interface e Fontes
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          font-name = "JetBrainsMono Nerd Font 11";
+          document-font-name = "JetBrainsMono Nerd Font 11";
+          monospace-font-name = "JetBrainsMono Nerd Font 11";
+          icon-theme = "Gruvbox-Plus-Dark";
+          gtk-theme = "Gruvbox-Dark-macos";
+        };
+
+        # Limpeza automática e Privacidade
+        "org/gnome/desktop/privacy" = {
+          remember-recent-files = true;
+          recent-files-max-age = 30;
+          remove-old-trash-files = true;
+          remove-old-temp-files = true;
+          old-files-age = 30;
+        };
+      };
+
+      # Configuração GTK
+      gtk = {
+        enable = true;
+        theme = {
+          name = "Gruvbox-Dark-macos";
+          package = gruvbox-theme-custom;
+        };
+        iconTheme = {
+          name = "Gruvbox-Plus-Dark";
+          package = pkgs.gruvbox-plus-icons;
+        };
+      };
+
+      # Links simbólicos para temas GTK4
+      home.file.".themes/Gruvbox-Dark-macos".source = "${gruvbox-theme-custom}/share/themes/Gruvbox-Dark-macos";
+      home.file.".local/share/icons/Gruvbox-Plus-Dark".source = "${pkgs.gruvbox-plus-icons}/share/icons/Gruvbox-Plus-Dark";
+
+      xdg.configFile = {
+        "gtk-3.0/settings.ini".text = ''
+          [Settings]
+          gtk-theme-name=Gruvbox-Dark-macos
+          gtk-icon-theme-name=Gruvbox-Plus-Dark
+          gtk-font-name=JetBrainsMono Nerd Font 11
+          gtk-cursor-theme-name=Adwaita
+          gtk-application-prefer-dark-theme=1
+        '';
+        "gtk-4.0/assets".source = "${gruvbox-theme-custom}/share/themes/Gruvbox-Dark-macos/gtk-4.0/assets";
+        "gtk-4.0/gtk.css".source = "${gruvbox-theme-custom}/share/themes/Gruvbox-Dark-macos/gtk-4.0/gtk.css";
+        "gtk-4.0/gtk-dark.css".source = "${gruvbox-theme-custom}/share/themes/Gruvbox-Dark-macos/gtk-4.0/gtk-dark.css";
+      };
+
+      # Variáveis de ambiente para GTK
+      home.sessionVariables = {
+        GTK_THEME = "Gruvbox-Dark-macos";
+      };
+
+      home.packages = with pkgs; [
+        jetbrains-mono
+        nerd-fonts.jetbrains-mono
+      ];
+    };
+
+  programs.firefox.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    ntfs3g
+    gnome-extension-manager
+    gnome-software
+    gnome-tweaks
+    gnomeExtensions.user-themes
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.dash-to-dock
+    gtk-engine-murrine
+    gnome-themes-extra
+    gruvbox-gtk-theme
+    gruvbox-plus-icons
+    sassc
+    vesktop
+    inter
+    wget
+    git
+    grc
+    vscode
+    vim
+    nixfmt
+    postgresql
+    ghostty
+    discord
+    element
+    spotify
+    vlc
+    obsidian
+    direnv
+    nodejs
+    fish
+    fishPlugins.done
+    fishPlugins.fzf-fish
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fishPlugins.grc
+  ];
+  environment.pathsToLink = [ "/share/applications" ];
+  system.stateVersion = "25.11";
+}
