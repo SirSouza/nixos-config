3ab69c9036576d62bc2bdbb9b131ecfc14f73c1d
SirSouza <sr.souza.contato@gmail.com>
2026-01-20 11:19:07 -0300

    feat: add VMM configuration

--- configuration.nix
+++ configuration.nix
@@ -295,9 +295,32 @@
   };
 
   # VM configuration
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+    };
+    onBoot = "start";
+    onShutdown = "shutdown";
+  };
   programs.virt-manager.enable = true;
 
+  #Network configuration
+  networking.firewall.checkReversePath = false;
+  environment.etc."libvirt/qemu/networks/default.xml".text = ''
+    <network>
+      <name>default</name>
+      <forward mode='nat'/>
+      <bridge name='virbr0' stp='on' delay='0'/>
+      <ip address='192.168.122.1' netmask='255.255.255.0'>
+        <dhcp>
+          <range start='192.168.122.2' end='192.168.122.254'/>
+        </dhcp>
+      </ip>
+    </network>
+  '';
   # Packages
   environment.systemPackages = with pkgs; [
     gsettings-desktop-schemas
