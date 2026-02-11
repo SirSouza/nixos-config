8bdb4afb41e113cc1a0f5db62b13087337b0a76c
SirSouza <sr.souza.contato@gmail.com>
2026-02-11 09:12:54 -0300

    refactor: extract system configuration to separate module

---
+++ modules/system.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Nix settings
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Hostname and networking
+  networking.hostName = "NixOS";
+  networking.networkmanager.enable = true;
+  networking.firewall.checkReversePath = false;
+
+  # Time zone
+  time.timeZone = "America/Sao_Paulo";
+
+  # Locale settings
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # System version
+  system.stateVersion = "25.11";
+}
