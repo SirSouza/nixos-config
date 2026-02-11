8a657fb47bc5bab6cb1a45e1c457fac4b2fa5740
SirSouza <sr.souza.contato@gmail.com>
2026-02-11 09:05:27 -0300

    Modularize: extract boot configurations

---
+++ modules/boot.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Bootloader
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 120;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # Lanzaboote for Secure Boot
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
+
+  # Generation limit
+  boot.loader.systemd-boot.configurationLimit = 3;
+
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+}
