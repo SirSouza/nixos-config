bf47c1c8b1aeba484c2a1b18a02643bb40049ca1
SirSouza <sr.souza.contato@gmail.com>
2026-02-11 09:44:17 -0300

    refactor: create modules

--- home.nix
+++ home.nix
@@ -1,46 +1,53 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
-let 
+let
   gruvbox-theme-custom = pkgs.gruvbox-gtk-theme.override {
     tweakVariants = [ "macos" ];
     colorVariants = [ "dark" ];
   };
-  in {
-    home.stateVersion = "25.11";
-    home.enableNixpkgsReleaseCheck = false;
+in
+{
+  home.stateVersion = "25.11";
+  home.enableNixpkgsReleaseCheck = false;
 
-    # Enable dconf (required for the configurations bellow)
-    dconf.enable = true;
+  # Enable dconf (required for the configurations bellow)
+  dconf.enable = true;
 
-    dconf.settings = {
-      # Interface and fonts
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-        font-name = "JetBrainsMono Nerd Font 11";
-        document-font-name = "JetBrainsMono Nerd Font 11";
-        monospace-font-name = "JetBrainsMono Nerd Font 11";
-        icon-theme = "Gruvbox-Plus-Dark";
-        gtk-theme = "Gruvbox-Dark"
-      };
-
-      # Auto cleaning and Privacy
-      "org/gnome/desktop/privacy" = {
-        remember-recent-files = true;
-        recent-files-max-age = 30;
-        remove-old-trash-files = true;
-        remove-old-temp-files = true;
-        old-files-age = 30;
-      };
+  dconf.settings = {
+    # Interface and fonts
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      font-name = "JetBrainsMono Nerd Font 11";
+      document-font-name = "JetBrainsMono Nerd Font 11";
+      monospace-font-name = "JetBrainsMono Nerd Font 11";
+      icon-theme = "Gruvbox-Plus-Dark";
+      gtk-theme = "Gruvbox-Dark";
     };
 
-   /*  gtk = {
+    # Auto cleaning and Privacy
+    "org/gnome/desktop/privacy" = {
+      remember-recent-files = true;
+      recent-files-max-age = 30;
+      remove-old-trash-files = true;
+      remove-old-temp-files = true;
+      old-files-age = 30;
+    };
+  };
+
+  /*
+    gtk = {
       enable = true;
       theme = {
         name = "Gruvbox-Dark"
       };
       icon-theme = {
         name = "Gruvbox-Plus-Dark";
-        package = pkgs.gruvbox-plus-icons;      
+        package = pkgs.gruvbox-plus-icons;
       };
       font {
       name = "JetBrainsMono Nerd Font";
@@ -52,15 +59,16 @@ let
       gtk4.extraConfig = {
         gtk-application-prefer-dark-theme = 1;
       };
-    }; */
-
-    # Enviroment Variables for GTK
-    home.sessionVariables = {
-      GTK_THEME = "Gruvbox-Dark";
     };
+  */
 
-    home.packages = with pkgs; [
-      jetbrains-mono
-      nerd-fonts.jetbrains-mono
-    ]
-  }
+  # Enviroment Variables for GTK
+  home.sessionVariables = {
+    GTK_THEME = "Gruvbox-Dark";
+  };
+
+  home.packages = with pkgs; [
+    jetbrains-mono
+    nerd-fonts.jetbrains-mono
+  ];
+}
